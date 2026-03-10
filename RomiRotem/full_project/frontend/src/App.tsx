daa07915d939858c92c9d0bcddf880f01b149d87
RomiRotem <romimagshimim@gmail.com>
2026-03-10 19:48:10 +0200

    up to render

--- RomiRotem/full_project/frontend/src/App.tsx
+++ RomiRotem/full_project/frontend/src/App.tsx
@@ -81,7 +81,7 @@ export const SettingsPanel = () => {
     };
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/predict", {
+      const response = await fetch("https://romirotem.onrender.com", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
